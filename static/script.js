e91aa633e9aa2f3c95c2ea5aad220b74fa2d70cd
Jesús Carpintero Rodríguez <j.carpintero@alumnos.upm.es>
2016-06-02 15:34:49 +0200

    Act

--- static/script.js
+++ static/script.js
@@ -3,7 +3,7 @@
   var height = api.tool.desktopHeight() - 70;
   var left   = ( wz.tool.environmentWidth() / 2 ) - ( width / 2 );
   var top    = ( wz.tool.environmentHeight() / 2 ) - ( height / 2 );
-  var windowObject = wz.popup( 'http://www.ebay.es', width, height ).render();
+  var windowObject = api.popup( 'http://www.ebay.es', width, height ).render();
 
     var timer = setInterval( function(){
 
